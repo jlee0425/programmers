9c5dae7d1b5f4ee9e934dab1939edb7856f8537b
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-10 21:47:47 +0900

    정확성 50/50; 효율성 50/50

---
+++ tested/완주못한선수.test.ts
@@ -0,0 +1,29 @@
+import { notCompleted } from '../CodingTest/Hash/완주못한선수';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('완주하지 못한 선수', () => {
+	it('["leo", "kiki", "eden"]	["eden", "kiki"]', () => {
+		expect(notCompleted(['leo', 'kiki', 'eden'], ['eden', 'kiki'])).to.eql(
+			'leo',
+		);
+	});
+
+	it('["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]', () => {
+		expect(
+			notCompleted(
+				['mislav', 'stanko', 'mislav', 'ana'],
+				['stanko', 'ana', 'mislav'],
+			),
+		).to.eql('mislav');
+	});
+
+	it('["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]', () => {
+		expect(
+			notCompleted(
+				['marina', 'josipa', 'nikola', 'vinko', 'filipa'],
+				['josipa', 'filipa', 'marina', 'nikola'],
+			),
+		).to.eql('vinko');
+	});
+});
