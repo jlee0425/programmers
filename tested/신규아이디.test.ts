a6a3cf3c1f1577066fea3ff0b7aa33493bd1a34d
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-10 17:11:07 +0900

    6, 18, 26 Not passed.

---
+++ tested/신규아이디.test.ts
@@ -0,0 +1,24 @@
+import { newid } from '../CodingTest/2021Kakao/신규아이디';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('신규 아이디 추천', () => {
+	it('"...!@BaT#*..y.abcdefghijklm"', () => {
+		expect(newid('...!@BaT#*..y.abcdefghijklm')).to.equal('bat.y.abcdefghi');
+	});
+	it('"z-+.^."', () => {
+		expect(newid('z-+.^.')).to.equal('z--');
+	});
+	it('"=.="', () => {
+		expect(newid('=.=')).to.equal('aaa');
+	});
+	it('"123_.def"', () => {
+		expect(newid('123_.def')).to.equal('123_.def');
+	});
+	it('"abcdefghijklmn.p"', () => {
+		expect(newid('abcdefghijklmn.p')).to.equal('abcdefghijklmn');
+	});
+	it('"aaaaaaaaaaaaaaaa"', () => {
+		expect(newid('aaaaaaaaaaaaaaaa')).to.equal('aaaaaaaaaaaaaaa');
+	});
+});
