8ce4536c98c993b939c61e350892fb967d02e316
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-11 16:14:07 +0900

    100/100

---
+++ tested/모의고사.test.ts
@@ -0,0 +1,17 @@
+import { supoja } from '../CodingTest/BruteForce/모의고사';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('모의고사', () => {
+	it('[1,2,3,4,5]', () => {
+		expect(supoja([1, 2, 3, 4, 5])).to.eql([1]);
+	});
+
+	it('[1,3,2,4,2]', () => {
+		expect(supoja([1, 3, 2, 4, 2])).to.eql([1, 2, 3]);
+	});
+
+	it('[0,0,0,0,0]', () => {
+		expect(supoja([0, 0, 0, 0, 0])).to.eql([1, 2, 3]);
+	});
+});
